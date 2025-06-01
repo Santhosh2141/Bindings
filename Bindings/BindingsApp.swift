614ca52b0d5845f21a6347eda656648ab5468014
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-01 19:42:50 +0530

    Initial Commit

---
+++ Bindings/BindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingsApp.swift
+//  Bindings
+//
+//  Created by Santhosh Srinivas on 01/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
