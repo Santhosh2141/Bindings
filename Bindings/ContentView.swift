031bd6c229dc397e8b8c5d98d67e8c6d401ed657
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-01 19:45:08 +0530

    Bindings - Custom variables to get and set data

--- Bindings/ContentView.swift
+++ Bindings/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Bindings
+//  Rock-Paper-Sciscors-Game
 //
 //  Created by Santhosh Srinivas on 01/06/25.
 //
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 0
+    @State var agreedToTerms = false
+    @State var agreedToPrivacyPolicy = false
+    @State var agreedToEmails = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        let binding = Binding(
+            get: { selection },
+            set: { selection = $0 }
+        )
+        let agreedToAll = Binding<Bool>(
+            get: {
+                agreedToTerms && agreedToPrivacyPolicy && agreedToEmails
+            },
+            set: {
+                agreedToTerms = $0
+                agreedToPrivacyPolicy = $0
+                agreedToEmails = $0
+            }
+        )
+        return VStack {
+            Picker("Select a number", selection: binding) {
+                ForEach(0..<3) {
+                    Text("Item \($0)")
+                }
+            }
+            .pickerStyle(.segmented)
+            Toggle("Agree to terms", isOn: $agreedToTerms)
+            Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
+            Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
+            Toggle("Agree to all", isOn: agreedToAll)
         }
-        .padding()
     }
 }
 
